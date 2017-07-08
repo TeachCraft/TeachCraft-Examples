2b4067d6cddacf11b1fdd021a99f7c56dee2f872
emeth- <seanybob@gmail.com>
2017-07-08 16:12:13 -0500

    cleanup

--- script.py
+++ script.py
@@ -1,25 +1,9 @@
 import mcpi.minecraft as minecraft
 import time
 
-# Connect to minecraft server 127.0.0.1 as player 'steve'
+# Connect to minecraft server 127.0.0.1 as player 'steve'. Replace with your own IP/name!
 mc = minecraft.Minecraft.create(address="127.0.0.1", name="steve")
-# HINT: Replace the IP and username above with your own!
 
-# Repeat every 0.2 seconds...
-while True:
+# PUT CODE HERE.
+# Checkout the /examples directory with ready-to-go scripts, all you need to do is update your IP and name in them!
 
-    #Retrieve the current player's X, Y, and Z coordinates
-    pos = mc.player.getPos()
-
-    #This is the minecraft block ID of the flower block.
-    #To see what other block IDs are available, go here in your browser: http://minecraft-ids.grahamedgecombe.com/
-    block = 38
-
-    #Set the block at the x/y/z coordinates of the current player to the block id we chose above.
-    mc.setBlock(pos.x, pos.y, pos.z, block)
-
-    time.sleep(0.2)
-
-
-# Checkout the Examples directoy for other examples you can run!
-# In each example, you'll need to update the IP Address and your game name.
