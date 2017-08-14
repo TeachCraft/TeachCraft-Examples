de456ff09a27cb7ab68634707114976d6dc89458
emeth- <seanybob@gmail.com>
2017-08-13 23:24:08 -0500

    fix

--- lesson1_setup.py
+++ lesson1_setup.py
@@ -7,5 +7,5 @@ mc = minecraft.Minecraft.create(address="127.0.0.1", name="steve")
 y = mc.getHeight(0, 0)
 mc.setBlocks(-4, y-1, -4, 4, y+1, 4, 7)
 mc.setBlocks(-3, y, -3, 3, y+20, 3, 0)
-print "In your Server console, please run the following command:"
-print "\tsetworldspawn 0 "+str(y)+" 0"
+print("In your Server console, please run the following command:")
+print("\tsetworldspawn 0 "+str(y)+" 0")
